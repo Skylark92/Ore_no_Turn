9774014db709e4d47a066f3a0e61228665e1f94a
Skylark92 <skylark92@live.co.kr>
2024-04-23 20:47:05 +0900

    새로운 카드 추가 시 애니메이션 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import 카드 from "./utils/Card";
 import Deck from "./components/Deck";
 import Card from "./components/Card";
@@ -7,21 +7,6 @@ import Header from "./components/Header";
 
 function App() {
   const [deck, setDeck] = useState<카드[]>([]);
-
-  useEffect(() => {
-    const newDeck = [];
-
-    for (let i = 0; i < 3; i++) {
-      newDeck.push(
-        new 카드(
-          `${i + 1}번 카드`,
-          `${i + 1}번 째`,
-          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
-        )
-      );
-    }
-    setDeck([...newDeck]);
-  }, []);
 
   return (
     <>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,39 @@
 import css from "./style.module.scss";
+import anim from "./anim.module.scss";
 import Back from "./Back";
 import Front from "./Front";
 import 카드 from "../../utils/Card";
+import { useEffect, useRef } from "react";
 
 export default function Card({ card, index }: { card: 카드; index: number }) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const { current } = ref;
+    const hoverOff = () => {
+      if (current) current.style.pointerEvents = "none";
+    };
+    const hoverOn = () => {
+      if (current) current.style.pointerEvents = "auto";
+    };
+
+    if (current) {
+      current.addEventListener("animationstart", hoverOff);
+      current.addEventListener("animationend", hoverOn);
+    }
+
+    return () => {
+      current?.removeEventListener("animationstart", hoverOff);
+      current?.removeEventListener("animationstart", hoverOn);
+    };
+  });
+
   return (
-    <div className={css.main} style={{ bottom: index * 5 + 15 + "px" }}>
+    <div
+      className={css.main + " " + anim.addCard}
+      style={{ bottom: index * 5 + 15 + "px" }}
+      ref={ref}
+    >
       <Front card={card} />
       <Back />
     </div>
